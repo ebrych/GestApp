97b06b61c75fc0b737266263a808654ee2757088
ebrych <ebrych@gmail.com>
2018-10-24 17:48:00 -0500

    Update Personal.php

--- application/controllers/Personal.php
+++ application/controllers/Personal.php
@@ -5,123 +5,35 @@ class Personal extends CI_Controller{
 
     public $controlador='1';
 
-    public function listPersonal(){
-        $this->load->model('DataModel');
-		$id = $this->input->post('id');
-        $token =$this->input->post('token');
-
-        //cargo-sesion
-        $cargo=$this->DataModel->obtenerCargo($id,$token);
-        if($cargo==null){
-            $data['datos']= null;
-        }else{
-            //permiso
-            $permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador);
-            if($permiso != 0){
-                $data['datos']= $this->DataModel->listaPersonal();
-            }else{
-                $data['datos']= null;
-            }
-        }
-        $this->load->view('index',$data);
+    public function index(){
+	$this->load->helper('url');
+	$this->load->model('DataModel');
+	$session=$this->session->userdata('logged');
+	$cargo=$this->session->userdata('cargo');
+	$permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador)
+	//sesion y permisos
+	if($session==true && $permiso != 0){
+		$info['MyNombre']=$this->session->userdata('username');
+		$data['personal']= $this->DataModel->listaPersonal();
+		$this->load->view('Layouts/header');
+		$this->load->view('Layouts/menu',$info);
+		$this->load->view('Personal/index',$data);
+		$this->load->view('Layouts/footer');	
+	}else{
+		redirect('http://'.base_url());
+	}
     }
-
-    public function agregarUsuario(){
-        $this->load->model('DataModel');
-		$id = $this->input->post('id');
-        $token =$this->input->post('token');
-
-        //cargo-sesion
-        $cargo=$this->DataModel->obtenerCargo($id,$token);
-        if($cargo==null){
-            $data['datos']= null;
-        }else{
-            //permiso
-            $permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador);
-            if($permiso != 0){
-                $nmbrs=$this->input->post('nombre');
-                $crg=$this->input->post('cargo');
-                $lcl=$this->input->post('local');
-                $ml=$this->input->post('email');
-                $tfn=$this->input->post('telefono');
-                $est=$this->input->post('estado');
-                $fech=date("Y-m-d");
-
-                $existe=$this->DataModel->buscaUsuario($ml);
-                if($existe==0){
-                    $data=array(
-                        'nombres' => $nmbrs,
-                        'idCargo' => $crg,
-                        'idLocal' => $lcl,
-                        'email' => $ml,
-                        'telefono' => $tfn,
-                        'pws' => $this->DataModel->generatePass(4),
-                        'fechaRegistro' => $fech,
-                        'estado'=>$est
-                    );
-                    $data['datos']= $this->DataModel->agregarUsuario($data);
-                }else{
-                    $data['datos']= null;
-                }
-                
-            }else{
-                $data['datos']= null;
-            }
-        }
-        $this->load->view('index',$data);
-    }
-
-    public function buscaPersonalById(){
-        $this->load->model('DataModel');
-		$id = $this->input->post('id');
-        $token =$this->input->post('token');
- 
-        //cargo-sesion
-        $cargo=$this->DataModel->obtenerCargo($id,$token);
-        if($cargo==null){
-            $data['datos']= null;
-        }else{
-            //permiso
-            $permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador);
-            if($permiso != 0){
-                $idUsr =$this->input->post('idUsuario');
-                $data['datos']= $this->DataModel->listaUsuarioById($idUsr);
-            }else{
-                $data['datos']= null;
-            }
-        }
-        $this->load->view('index',$data);
-    }
-
-    public function updatePersonal(){
-        $this->load->model('DataModel');
-		$id = $this->input->post('id');
-        $token =$this->input->post('token');
- 
-        //cargo-sesion
-        $cargo=$this->DataModel->obtenerCargo($id,$token);
-        if($cargo==null){
-            $data['datos']= null;
-        }else{
-            //permiso
-            $permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador);
-            if($permiso != 0){
-                $idUsr =$this->input->post('idUsuario');
-                $datos=array(
-                    'nombres' => $this->input->post('nombres'),
-                    'idCargo' => $this->input->post('cargo'),
-                    'idLocal' => $this->input->post('local'),
-                    'email' => $this->input->post('email'),
-                    'telefono' => $this->input->post('telefono'),
-                    'estado' =>  $this->input->post('estado')
-                );
-                $data['datos']= $this->DataModel->actualizaUsuario($idUsr,$datos);
-            }else{
-                $data['datos']= null;
-            }
-        }
-        $this->load->view('index',$data);
-    } 
 
 
 }
+
+
+
+/*
+$cargo=$this->session->userdata('cargo');
+	$permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador)
+	//sesion y permisos
+	if($session==true && $permiso != 0){
+
+	}
+*/
