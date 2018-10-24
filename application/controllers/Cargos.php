fd813cf0b58a9bcc2836337acbc0c4b82d4c31f1
ebrych <ebrych@gmail.com>
2018-10-24 18:08:35 -0500

    Update Cargos.php

--- application/controllers/Cargos.php
+++ application/controllers/Cargos.php
@@ -7,7 +7,22 @@ class Cargos extends CI_Controller {
     
     
     public function index(){
-        
+        $this->load->helper('url');
+        $this->load->model('DataModel');
+        $session=$this->session->userdata('logged');
+        $cargo=$this->session->userdata('cargo');
+        $permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador)
+        //sesion y permisos
+        if($session==true && $permiso != 0){
+            $info['MyNombre']=$this->session->userdata('username');
+            $data['cargos']= $this->DataModel->cargosGroupList();
+            $this->load->view('Layouts/header');
+            $this->load->view('Layouts/menu',$info);
+            $this->load->view('Cargos/index',$data);
+            $this->load->view('Layouts/footer');	
+        }else{
+            redirect('http://'.base_url());
+        }
     }
     
 
