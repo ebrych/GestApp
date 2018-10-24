238ac85c2311a4a87fe196c70c09eae44ff18ae5
ebrych <ebrych@gmail.com>
2018-10-24 18:06:00 -0500

    Update Cargos.php

--- application/controllers/Cargos.php
+++ application/controllers/Cargos.php
@@ -3,7 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cargos extends CI_Controller {
 
-
+    public $controlador='4';
+    
+    
+    public function index(){
+        
+    }
     
 
 }
