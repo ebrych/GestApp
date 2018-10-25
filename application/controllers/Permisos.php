f269d7b728a44500a21842597d91d30284e0974a
ebrych <ebrych@gmail.com>
2018-10-25 16:01:05 -0500

    Update Permisos.php

--- application/controllers/Permisos.php
+++ application/controllers/Permisos.php
@@ -2,4 +2,82 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Permisos extends CI_Controller{
 
+  public function index(){
+        $this->load->helper('url');
+        $this->load->model('DataModel');
+        $session=$this->session->userdata('logged');
+        $cargo=$this->session->userdata('cargo');
+        $permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador)
+        //sesion y permisos
+        if($session==true && $permiso != 0){
+            $info['MyNombre']=$this->session->userdata('username');
+            $data['cargos']= $this->DataModel->cargosGroupList();
+            $this->load->view('Layouts/header');
+            $this->load->view('Layouts/menu',$info);
+            $this->load->view('Permisos/index',$data);
+            $this->load->view('Layouts/footer');	
+        }else{
+            redirect(base_url());
+        }
+  }
+  
+  public function permisoCargo($cargo_id){
+        $this->load->helper('url');
+        $this->load->model('DataModel');
+        $session=$this->session->userdata('logged');
+        $cargo=$this->session->userdata('cargo');
+        $permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador)
+        //sesion y permisos
+        if($session==true && $permiso != 0){
+            $info['MyNombre']=$this->session->userdata('username');
+            $data['miCargo'] = $cargo_id;
+            $data['misPermisos']= $this->DataModel->listaPermisosById($cargo_id);
+            $data['permisos']=$this->DataModel->listarPermisos();
+            $this->load->view('Layouts/header');
+            $this->load->view('Layouts/menu',$info);
+            $this->load->view('Permisos/permisoCargo',$data);
+            $this->load->view('Layouts/footer');	
+        }else{
+            redirect(base_url());
+        }
+  }
+  
+  public function agregaPermiso(){
+        $this->load->helper('url');
+        $this->load->model('DataModel');
+        $session=$this->session->userdata('logged');
+        $cargo=$this->session->userdata('cargo');
+        $permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador)
+        //sesion y permisos
+        if($session==true && $permiso != 0){
+            $crgo=$this->input->post('cargo');
+            $prms=$this->input->post('permiso');
+            $data=array(
+                        'idCargo' => $crgo,
+                        'idPermiso' => $prms
+            );
+            $this->DataModel->insertaPermiso($data);
+            redirect(base_url()+"Permisos/permisoCargo/"+$crgo);
+        }else{
+            redirect(base_url());
+        }
+  }
+  
+  public function eliminaPermiso($cargo_id,$permiso_id){
+        $this->load->helper('url');
+        $this->load->model('DataModel');
+        $session=$this->session->userdata('logged');
+        $cargo=$this->session->userdata('cargo');
+        $permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador)
+        //sesion y permisos
+        if($session==true && $permiso != 0){
+            $data['datos']= $this->DataModel->eliminaPermiso($cargo_id,$permiso_id);
+            redirect(base_url()+"Permisos/permisoCargo/"+$crgo);
+        }else{
+            redirect(base_url());
+        }
+  }
+  
+  
+  
 }
