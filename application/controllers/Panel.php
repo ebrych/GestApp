da9a73fced8b8e6f908f6aa22db5c9f45142fe4b
ebrych <ebrych@gmail.com>
2018-10-26 16:03:45 -0500

    Update Panel.php

--- application/controllers/Panel.php
+++ application/controllers/Panel.php
@@ -5,11 +5,10 @@ class Panel extends CI_Controller {
 
 	public function Permisos()
 	{
-		$this->load->model('DataModel');
 		$id = $this->input->post('id');
-        $token =$this->input->post('token');
-        $cargo=$this->DataModel->obtenerCargo($id,$token);
-        $data['datos']=$this->DataModel->obtenerPermisos($cargo);
+        	$token =$this->input->post('token');
+        	$cargo=$this->DataModel->obtenerCargo($id,$token);
+        	$data['datos']=$this->DataModel->obtenerPermisos($cargo);
 		$this->load->view('index',$data);
 	}
 
