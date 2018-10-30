b1406f37d1dd45b896fff0c0faeae4bc1ce9dc28
ebrych <ebrych@gmail.com>
2018-10-30 17:49:41 -0500

    Update permisos.php

--- application/views/Cargos/permisos.php
+++ application/views/Cargos/permisos.php
@@ -1,1 +1,8 @@
+<p>&nbsp;</p>
 
+<div class="container">
+  <h1>Permisos del cargo</h1> 
+  <a href="<?php echo base_url() ?>Cargos/" style="font-size:20px;color:#007bff;"><i class="fas fa-arrow-left"></i> Regresar </a>
+  <p>&nbsp;</p>
+  
+</div>
