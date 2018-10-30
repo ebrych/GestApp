2e6f323b2e3e613e0f1e0a72fd73c8f56abd0ad9
ebrych <ebrych@gmail.com>
2018-10-30 18:06:22 -0500

    Update permisos.php

--- application/views/Cargos/permisos.php
+++ application/views/Cargos/permisos.php
@@ -4,5 +4,19 @@
   <h1>Permisos del cargo</h1> 
   <a href="<?php echo base_url() ?>Cargos/" style="font-size:20px;color:#007bff;"><i class="fas fa-arrow-left"></i> Regresar </a>
   <p>&nbsp;</p>
-  
+  <form>
+    <input class="form-control" name="cargo" value="<?php echo $cargoId; ?>" style="display: none" required >
+    <lable>Nombre del Cargo:</lable>
+    <input class="form-control" name="nombreCargo" value="<?php echo $cargoNombre; ?>" disabled required >
+    <lable>Permisos</lable>
+    <select class="form-control" name="permiso"  required>
+      <?php
+          foreach ($locales as $row){
+      ?>
+        <option  value="<?php echo $row->id ?>"><?php echo $row->descripcion ?></option>
+      <?php 
+         } 
+      ?>
+     </select>
+  </form>
 </div>
