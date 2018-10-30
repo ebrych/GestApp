253caf7aa5c7c765d0f778c53d2547015dd8fdb3
ebrych <ebrych@gmail.com>
2018-10-30 17:17:28 -0500

    Update actualiza.php

--- application/views/Cargos/actualiza.php
+++ application/views/Cargos/actualiza.php
@@ -4,11 +4,12 @@
   <h1>Actualiza Cargo</h1> 
   <a href="<?php echo base_url() ?>Cargos/" style="font-size:20px;color:#007bff;"><i class="fas fa-arrow-left"></i> Regresar </a>
   <p>&nbsp;</p>
-        <form action="<?php echo base_url() ?>Cargos/agregarCargo" method="post">
+        <form action="<?php echo base_url() ?>Cargos/actualizaDato" method="post">
+              <input type="text" class="form-control" name="cargoId" value="<? echo cargo[0]->cargoId ?>" placeholder="Nombres del Cargo" required >
               <label>Descripción</label>
-              <input type="text" class="form-control" name="descripcion" id="descripcion" placeholder="Nombres del Cargo" required >
+              <input type="text" class="form-control" name="descripcion" value="<? echo cargo[0]->cargoDescrip ?>" placeholder="Nombres del Cargo" required >
               <label>Estado</label>
-              <select class="form-control" name="estado" id="estado" required >
+              <select class="form-control" name="estado" value="<? echo cargo[0]->cargoEstId ?>" required >
                   <option value="1">Activo</option>
                   <option value="0">Inactivo</option>
               </select><br/>
