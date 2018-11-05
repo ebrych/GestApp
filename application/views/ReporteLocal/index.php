7eb1fdaf2946c77ebfb7a497d30ce63e9a65f8b8
ebrych <ebrych@gmail.com>
2018-11-05 12:00:27 -0500

    Update index.php

--- application/views/ReporteLocal/index.php
+++ application/views/ReporteLocal/index.php
@@ -1,1 +1,43 @@
-reporte local
+<br/>
+<div class="container">
+  <h1>Reporte de Locales</h1>
+  <div class="row">
+        <div class="col-sm">
+            <form action="<?php echo base_url() ?>ReporteLocal/index" method="get" >
+            <div class="input-group">
+                <input type="date" class="form-control" name="dateData" value="<?php echo $hoy; ?>">
+                <div class="input-group-append">
+                  <button class="btn btn-primary" type="submit" id="button-addon3">Buscar</button>
+                </div>
+              </div>
+            </form>
+        </div>
+        <div class="col-sm"></div> 
+        <div class="col-sm" align="right">
+          filtros
+        </div>
+  </div>
+  <div class="list-group">
+        <?php
+            foreach ($locales as $row){
+        ?>
+        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="row">
+                <div class="col-8">
+                    <h5 class="nombre" style="font-weight: bold;font-size:25px"> <?php echo $row->nombre ?></h5>
+                    <i class="fas fa-map-marker"></i> <?php echo $row->direccion ?>
+                    <i class="far fa-clock"></i> Actividad del mes: <?php echo $row->actividad ?>
+                </div>
+                <div class="col-4" align="right">
+                  &nbsp;
+                </div>
+            </div>
+        </a>
+        <?php 
+            } 
+        ?>
+        
+    </div>
+  
+</div>
+
