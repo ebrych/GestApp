5a1dae06747a4a6e256841133ccb01f9cf44c2a9
ebrych <ebrych@gmail.com>
2018-11-05 16:17:59 -0500

    Update index.php

--- application/views/Web/index.php
+++ application/views/Web/index.php
@@ -18,30 +18,63 @@
             <div style="font-size:20px;color:#007bff;cursor: pointer;" align="right" (click)="abrirModalNuevaEntrada()"><i class="fas fa-plus-circle"></i> Agregar entrada </div>
             <p>&nbsp;</p>
             <?php
-              foreach()
+              foreach($informacion as $row)
             ?>
-            <div class="container" *ngFor="let data of EntradasList">
+            <div class="container">
               <div class="row" >
                 <div class="col-sm-2">
-                  Titulo:<br/> {{data.titulo}}
+                  Titulo:<br/> <?php echo $row->titulo ?>
                 </div>
                 <div class="col-sm-6">
-                  Descripción:<br/>  {{data.descripcion}}
+                  Descripción:<br/> <?php echo $row->descripcion ?>
                 </div>
                 <div class="col-sm-2">
-                  Orden:  {{data.orden}}
+                  Orden: <?php echo $row->orden ?>
                 </div>
                 <div class="col-sm-2" align="right">
                   <div style="color:#007bff;cursor: pointer;">
-                      <i class="fas fa-pencil-alt"  (click)="editEntrada(data.id)" style="font-size:20px;"></i>
-                      &nbsp;
-                      <i class="fas fa-trash-alt" (click)="deleteEntrada(data.id)" style="font-size:20px;"></i>
+                      <a href="<?php base_url() ?>Web/eliminarInformacion/<?php echo $row->id ?>" style="font-size:20px;"><i class="fas fa-trash-alt"></i></a>
                   </div>
                 </div>
               </div>
               <hr/>
+              <?php 
+              }
+              ?>
             </div>
-
           </div>
+            
+        <div class="card">
+        <div class="card-header" id="headingTwo">
+          <h5 class="mb-0">
+            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              <i class="fas fa-mail-bulk" style="font-size:22px"></i> &nbsp;&nbsp;CONTÁCTO
+            </button>
+          </h5>
+        </div>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+          <div class="card-body">
+            <div class="container">
+                <div class="row">
+                    <form action="<?php echo base_url() ?>Web/actualizaContacto" method="post" >
+                        <label>Telefono</label>
+                        <input type="text" class="form-control" name="numero" value="<?php echo $contacto['numero'] ?>"  required >
+                        <label>Email</label>
+                        <input type="email" class="form-control" name="email" value="<?php echo $contacto['mail'] ?>" required >
+                        <label>Dirección de Facebook</label>
+                        <input type="text" class="form-control" name="facebook" value="<?php echo $contacto['facebook'] ?>" required >
+                        <label>Dirección de Instagram</label>
+                        <input type="text" class="form-control" name="instagram" value="<?php echo $contacto['instagram '] ?>" required >
+                        <br/>
+                        <button class="btn btn-primary btn-block" type="submit">Guardar</button> 
+                    </form>
+                </div>
+            </div>
+          </div>
+        </div>
+</div>   
+            
+            
+            
         </div>
 </div>
