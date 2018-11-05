93470802d2aa919615efabeb6d30cb5788a459c5
ebrych <ebrych@gmail.com>
2018-11-05 09:07:01 -0500

    Update index.php

--- application/views/Reservas/index.php
+++ application/views/Reservas/index.php
@@ -1,1 +1,7 @@
-
+<br/>
+<div class="container">
+  <h1>Reservas</h1> 
+    <a href="<?php echo base_url() ?>Reservas/nuevo" style="font-size:20px;color:#007bff;"><i class="fas fa-plus-circle"></i> Agregar Reserva</a>
+    <p>&nbsp;</p>
+  
+</div>
