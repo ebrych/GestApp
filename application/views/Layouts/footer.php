45f24e2b0e6cee67141a90e347e13a6025c115fd
ebrych <ebrych@gmail.com>
2018-11-05 16:41:01 -0500

    Update footer.php

--- application/views/Layouts/footer.php
+++ application/views/Layouts/footer.php
@@ -1,7 +1,4 @@
     
-    <script src="http://localhost/GestApp/assets/js/jquery.min.js" ></script>
-    <script src="http://localhost/GestApp/assets/js/bootstrap.min.js" ></script>
-    <script src="http://localhost/GestApp/assets/js/list.min.js"></script>
-    <script src="http://localhost/GestApp/assets/js/bootstrap-select.min.js"></script>
+    
   </body>
 </html>
