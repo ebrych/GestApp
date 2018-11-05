c206fc32e5ff13ca7be1f1072e2bf25699291570
ebrych <ebrych@gmail.com>
2018-11-05 12:27:00 -0500

    Update Productividad.php

--- application/controllers/Productividad.php
+++ application/controllers/Productividad.php
@@ -3,18 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Productividad extends CI_Controller{
     public $controlador='11';
     
-    public function index(){
+    public function index($fecha=null){
         $session=$this->session->userdata('logged');
         $cargo=$this->session->userdata('cargo');
         $permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador)
         //sesion y permisos
         if($session==true && $permiso != 0){
-            $hoy=$this->input->post('date');
+            $fecha=$this->input->get('dateData');
+            if($fecha==null){
+                $fecha=date("Y-m-d");
+            }
             $dataFecha=EXPLODE("-",$hoy);
-            $data['personal']= $this->DataModel->listaTrabajadorReporte($dataFecha[1]);  
-            $info['MyNombre']=$this->session->userdata('username');    
+            $data['personal']= $this->DataModel->listaTrabajadorReporte($dataFecha[1]);   
             $this->load->view('Layouts/header');
-            $this->load->view('Layouts/menu',$info);
+            $this->load->view('Layouts/menu');
             $this->load->view('Productividad/index',$data);
             $this->load->view('Layouts/footer');	
         }else{
