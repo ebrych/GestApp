78843a2affa2f039362c4c01bdf52cd9c995a293
ebrych <ebrych@gmail.com>
2018-11-05 17:26:41 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
-//myApp
+/*
+╔═════════════════════════╗ 
+║{7=+A(L8Wc)pnsQ4:startJs ║ 
+╚═════════════════════════╝
+*/
+//====================================Cargos
+var cargosOption = {
+    valueNames: [ 'nombre', 'estado' ],
+    page: 5,
+    pagination: true
+};
+var cargosList = new List('cargos-list', cargosOption);
