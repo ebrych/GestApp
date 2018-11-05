d81b5e591b385e3de208b6db5faff7e0904586d7
ebrych <ebrych@gmail.com>
2018-11-05 11:58:58 -0500

    Update ReporteLocal.php

--- application/controllers/ReporteLocal.php
+++ application/controllers/ReporteLocal.php
@@ -4,14 +4,17 @@ class ReporteLocal extends CI_Controller{
     
     public $controlador='12';
     
-    public function index(){
+    public function index($fecha=null){
         $session=$this->session->userdata('logged');
         $cargo=$this->session->userdata('cargo');
         $permiso=$this->DataModel->veryfyPermission($cargo,$this->controlador);
         //sesion y permisos
         if($session==true && $permiso != 0){
-            $hoy=$this->input->post('date');
-            $dataFecha=EXPLODE("-",$hoy);
+            $fecha=$this->input->get('dateData');
+            if($fecha==null){
+                $fecha=date("Y-m-d");
+            }
+            $dataFecha=EXPLODE("-",$fecha);
             $data['locales']= $this->DataModel->listaLocalesReporte($dataFecha[1]);    
             $this->load->view('Layouts/header');
             $this->load->view('Layouts/menu');
