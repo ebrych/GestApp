509ae0154f23e1a2eaf517aa7b2e99db85ba8c3b
ebrych <ebrych@gmail.com>
2018-11-06 14:56:34 -0500

    Update MesajesModel.php

--- application/models/MesajesModel.php
+++ application/models/MesajesModel.php
@@ -6,4 +6,27 @@ class MesajesModel extends CI_Model
     {
         parent::__construct(); // construct the Model class
     }
+    
+   public function enviarMensaje($mailTo,$asunto,$mensaje){
+        $config = array(
+                 'protocol' => 'smtp',
+                 'smtp_host' => 'smtp.live.com',
+                 'smtp_port' => 465,
+                 'smtp_user' => 'edificiotradiciones@hotmail.com',
+                 'smtp_pass' => 'tradiciones2017',
+                 'mailtype' => 'html',
+                 'charset' => 'utf-8',
+                 'newline' => "\r\n"
+                 );
+        $this->load->library('email',$config);
+        $this->email->set_mailtype('html');
+        $this->email->from('edificiotradiciones@hotmail.com');
+        $this->email->to($mailTo);
+        $this->email->subject($asunto);
+        $this->email->message($mensaje);
+        $this->email->send();
+   }
+    
+    
+    
 }
