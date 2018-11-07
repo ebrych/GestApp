00a7bf1546a13b6ad8ea139ae0bac3b866706f49
ebrych <ebrych@gmail.com>
2018-11-07 12:55:05 -0500

    Update index.php

--- application/views/Reservas/index.php
+++ application/views/Reservas/index.php
@@ -1,9 +1,21 @@
 <br/>
-<div class="container">
+<div class="container" id="list">
   <h1>Reservas</h1> 
   <p>&nbsp;</p>
+   <div class="row">
+        <div class="col col-lg-6" style="padding:0px"></div>
+        <div class="col col-lg-6" style="padding:0px">
+          <div class="input-group mb-2 mr-sm-2">
+          <div class="input-group-prepend">
+            <div class="input-group-text" style="background-color:#ffffff"><i class="fas fa-search"></i></div>
+          </div>
+          <input type="text" class="form-control search" placeholder="Filtrar">
+          </div>
+        </div>
+    </div>
   
   <div class="list-group">
+        <div class="list">
         <?php
             foreach ($reservas as $row){
         ?>
@@ -24,7 +36,9 @@
         </a>
         <?php 
             } 
-        ?>  
+        ?>
+          <div align="right"><ul class="pagination"></ul></div>
+        </div>
     </div>
 </div>
 
@@ -36,4 +50,11 @@
         location.href="<?php echo base_url() ?>Reservas/cancelaReserva/"+id;
       }
   }
+  //listJS
+  var options = {
+  valueNames: [ 'nombre' ],
+  page: 4,
+  pagination: true
+  };
+  var listJs = new List('list', options);
 </script>
