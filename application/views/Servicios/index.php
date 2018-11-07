2a07787b701636381bc81324784cf5b4985c51a8
ebrych <ebrych@gmail.com>
2018-11-07 14:20:03 -0500

    Update index.php

--- application/views/Servicios/index.php
+++ application/views/Servicios/index.php
@@ -1,10 +1,22 @@
-<p>&nbsp;</p>
-<div class="container">
+<br/>
+<div class="container" id="list">
   <h1>Servicios</h1> 
     <a href="<?php echo base_url() ?>Servicios/nuevo" style="font-size:20px;color:#007bff;"><i class="fas fa-plus-circle"></i> Agregar Cargo</a>
     <p>&nbsp;</p>
+  <div class="row">
+        <div class="col col-lg-6" style="padding:0px"></div>
+        <div class="col col-lg-6" style="padding:0px">
+          <div class="input-group mb-2 mr-sm-2">
+          <div class="input-group-prepend">
+            <div class="input-group-text" style="background-color:#ffffff"><i class="fas fa-search"></i></div>
+          </div>
+          <input type="text" class="form-control search" placeholder="Filtrar">
+          </div>
+        </div>
+    </div>
   
     <div class="list-group">
+        <div class="list">
         <?php
             foreach ($servicios as $row){
         ?>
@@ -25,6 +37,8 @@
         <?php 
             } 
         ?>
+         <div align="right"><ul class="pagination"></ul></div>
+        </div>
     </div>
   
 </div>
@@ -34,4 +48,11 @@
   function hrefToAct(id){
     location.href="<?php echo base_url() ?>Servicios/actualiza/"+id;   
   }
+   //listJS
+  var options = {
+  valueNames: [ 'nombre' ],
+  page: 4,
+  pagination: true
+  };
+  var listJs = new List('list', options);
 </script>
