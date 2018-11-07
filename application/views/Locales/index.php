e557dd8ea882911ff6e667296a82af82b0828f50
ebrych <ebrych@gmail.com>
2018-11-07 12:38:23 -0500

    Update index.php

--- application/views/Locales/index.php
+++ application/views/Locales/index.php
@@ -1,10 +1,23 @@
 <br/>
-<div class="container">
+<div class="container" id="list">
   <h1>Locales</h1> 
     <a href="<?php echo base_url() ?>Locales/nuevo" style="font-size:20px;color:#007bff;"><i class="fas fa-plus-circle"></i> Agregar Local</a>
     <p>&nbsp;</p>
+   <div class="row">
+        <div class="col col-lg-6" style="padding:0px"></div>
+        <div class="col col-lg-6" style="padding:0px">
+          <div class="input-group mb-2 mr-sm-2">
+          <div class="input-group-prepend">
+            <div class="input-group-text" style="background-color:#ffffff"><i class="fas fa-search"></i></div>
+          </div>
+          <input type="text" class="form-control search" placeholder="Filtrar">
+          </div>
+        </div>
+    </div>
   
+    
     <div class="list-group">
+        <div class="list">
         <?php
             foreach ($locales as $row){
         ?>
@@ -24,7 +37,7 @@
         <?php 
             } 
         ?>
-        
+        </div>
     </div>
   
 </div>
@@ -36,4 +49,11 @@
         location.href="<?php echo base_url() ?>Locales/actualiza/"+id;
       }
   }
+  //listJS
+  var options = {
+  valueNames: [ 'nombre' ],
+  page: 4,
+  pagination: true
+  };
+  var listJs = new List('list', options);
 </script>
