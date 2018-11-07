e549c9f200a4216890c4fda0e74fe9f5a67b28e9
ebrych <ebrych@gmail.com>
2018-11-07 12:43:29 -0500

    Update index.php

--- application/views/Marcaciones/index.php
+++ application/views/Marcaciones/index.php
@@ -1,8 +1,19 @@
 <br/>
-<div class="container">
+<div class="container" id="list">
 <h1>Personal</h1>
-
+    <div class="row">
+        <div class="col col-lg-6" style="padding:0px"></div>
+        <div class="col col-lg-6" style="padding:0px">
+          <div class="input-group mb-2 mr-sm-2">
+          <div class="input-group-prepend">
+            <div class="input-group-text" style="background-color:#ffffff"><i class="fas fa-search"></i></div>
+          </div>
+          <input type="text" class="form-control search" placeholder="Filtrar">
+          </div>
+        </div>
+    </div>
   <div class="list-group">
+        <div class="list">
         <?php
             foreach ($personal as $row){
         ?>
@@ -21,6 +32,7 @@
         <?php 
             } 
         ?>
+        </div>
     </div>
 
 </div>
@@ -29,4 +41,11 @@
   function hrefToAct(id){
     location.href="<?php echo base_url() ?>Marcaciones/agregarMarcacion/"+id;
   }
+  //listJS
+  var options = {
+  valueNames: [ 'nombre' ],
+  page: 4,
+  pagination: true
+  };
+  var listJs = new List('list', options);
 </script>
